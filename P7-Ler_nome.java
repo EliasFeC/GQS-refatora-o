f1c778670a69964c8893b6fc01836330e1d07cd2
EliasFeC <151461511+EliasFeC@users.noreply.github.com>
2024-06-26 18:51:32 -0300

    Update P7-Ler_nome.java

--- P7-Ler_nome.java
+++ P7-Ler_nome.java
@@ -3,11 +3,11 @@ import javax.swing.JOptionPane;
 public class NomeCompleto {
     public static void main(String[] args) {
         String primeiroNome = JOptionPane.showInputDialog("Digite o primeiro nome:");
-        String nomeMeio = JOptionPane.showInputDialog("Digite o nome do meio:");
+        String nomeDoMeio = JOptionPane.showInputDialog("Digite o nome do meio:");
         String ultimoNome = JOptionPane.showInputDialog("Digite o último nome:");
 
-        String nomeCompleto = primeiroNome + " " + nomeMeio + " " + ultimoNome;
+        String nomeCompleto = primeiroNome + " " + nomeDoMeio + " " + ultimoNome;
 
-        JOptionPane.showMessageDialog(null, "O nome completo é: " + nomeCompleto);
+        JOptionPane.showMessageDialog(null, "Nome completo: " + nomeCompleto);
     }
 }
